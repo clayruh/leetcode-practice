893dd9d120bb4c5bfb9f2ad56b8648351b875fd4
Clae <56447997+clayruh@users.noreply.github.com>
2024-03-31 23:02:46 -0400

    working solution for problem 2089

---
+++ arrays/leetcode_2089.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def targetIndices(self, nums, target):
+        targetIndex = []
+        nums.sort()
+        for i in range(len(nums)):
+            if nums[i] == target:
+                targetIndex.append(i)
+        return targetIndex
