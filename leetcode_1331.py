a38f615fbc078b31d6786110ca91d508fc1da33c
Clae <56447997+clayruh@users.noreply.github.com>
2024-04-03 16:50:48 -0400

    not sure how to approach problem 219

--- leetcode_1331.py
+++ leetcode_1331.py
@@ -1,4 +1,4 @@
 class Solution(object):
     def arrayRankTransform(self, arr):
-
+        
         return
---
+++ unsolved/leetcode_219.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def containsNearbyDuplicate(self, nums, k):
+
+
+
+
+        # if abs(i - j) <= k:
+        #   return True
+        return
