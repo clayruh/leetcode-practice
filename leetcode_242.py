3b941f460d0615fd4f43432802120b0383c188e9
Clae <56447997+clayruh@users.noreply.github.com>
2024-04-24 00:13:22 -0400

    problem 242 with working solution! YAY

--- leetcode_242.py
+++ leetcode_242.py
@@ -7,5 +7,23 @@ def isAnagram(s,t):
     # else:
     # for every letter in t that matches s, remove the letter
     # once len(s_as_array) = 0 we know all the letters of t match letters of s
-        
+    s_as_array = list(s)
+    t_as_array = list(t)
+
+    while len(s_as_array) > 0:
+        for letter in t_as_array:
+            # print(f'{letter}')
+            if letter not in s_as_array or len(t_as_array) < len(s_as_array):
+                # print(f'false')
+                return False
+            else:
+                # print(f'removing letter {letter} from s')
+                # print(f't: {t_as_array} s: {s_as_array}')
+                s_as_array.remove(letter)
+    # print(f'true')
     return True
+
+s = "aa"
+t = "a"
+
+isAnagram(s,t)
