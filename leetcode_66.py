00d3c34051ed408325532fd6d8e266b85a5acc2e
Clae <56447997+clayruh@users.noreply.github.com>
2024-05-27 17:51:22 -0400

    working solution

--- leetcode_66.py
+++ leetcode_66.py
@@ -1,3 +1,26 @@
 def PlusOne(digits):
-    
-    return
+    # if the last digit in the list isn't a 9,
+    # digits[-1] += 1
+    if digits[-1] != 9:
+        digits[-1]+= 1
+        return digits
+    # from here we can make the list into an actual number 
+    # then +1, and remake into a list
+    multiplier = 1
+    sum_of_digits_list = 0
+    for digit in digits[::-1]:
+        sum_of_digits_list += digit * multiplier
+        multiplier *= 10
+    print(sum_of_digits_list)
+
+    sum_of_digits_list += 1
+    number_as_list = []
+
+    while sum_of_digits_list > 0:
+        digit = sum_of_digits_list % 10 
+        number_as_list.append(digit)
+        sum_of_digits_list //= 10
+    print(number_as_list[::-1])
+    return number_as_list[::-1]
+
+PlusOne([5,9,9,9,9,8,9])
