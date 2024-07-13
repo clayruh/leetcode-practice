7444e540cc01bde4c7b74e149713899e81484210
Clae <56447997+clayruh@users.noreply.github.com>
2024-07-13 16:28:58 -0400

    unsolved problem 1512

---
+++ unsolved/leetcode_1512.py
@@ -0,0 +1,14 @@
+def numIdenticalPairs(nums):
+    good_pairs = 0
+    # map each number to an index into a dictionary {num : [indexes where the num shows up in nums]}
+        # {1:[0, 3, 4], 3:[2, 5]}
+        # {1:[0, 1, 2, 3]}
+    # then a for loop through the dictionary or nums
+    # if the length of the array at the value is > 1,
+    # do something lol
+
+    # other option is nested for loop? after the dict creation
+    # for i in range(len(nums)): 
+    # if nums[i]
+    return
+        
