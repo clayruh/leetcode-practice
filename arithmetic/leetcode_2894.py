5314c3a9f938183ae74c69f3c64d26a652cf11e4
Clae <56447997+clayruh@users.noreply.github.com>
2024-07-14 22:47:16 -0400

    two solutions for 2894

--- arithmetic/leetcode_2894.py
+++ arithmetic/leetcode_2894.py
@@ -1,12 +1,16 @@
 def differenceOfSums(n: int, m: int):
     # num1 is the sum of all integers in range [1,n] not divisible by m
     # num2 is sum of all integers [1,n] divisible by m
-
-    # for i in range(n):
-    #     num1 = sum(i for i in n if n % m != 0)
-    
-    # for i in range(m):
-    #     num2 = sum(i for i in n if n % m == 0)
+    # num1_arr = []
+    # num2_arr = []
+    # for i in range(n+1):
+    #     if i % m != 0:
+    #         num1_arr.append(i) 
+    #     if i % m == 0:
+    #         num2_arr.append(i)
+    # num1 = sum(n for n in num1_arr)
+    # num2 = sum(n for n in num2_arr)
+    # print(num1_arr,num2_arr)
 
     num1 = 0
     num2 = 0
@@ -17,4 +21,4 @@ def differenceOfSums(n: int, m: int):
             num2 += i
     return num1 - num2
         
-print(differenceOfSums(10,3))
+print(differenceOfSums(5,6))
